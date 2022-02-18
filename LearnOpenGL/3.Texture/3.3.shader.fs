8954923d353933ea08d62e15b77361bb666931ae
huzhihao <1162287492@qq.com>
2022-02-18 17:37:33 +0800

    3.Texture.exercise-1,镜面变换笑脸纹理方向,分别设置两组Texture_Coord, 更简单的办法是直接在FragmentShader中mix的时候采样镜面位置的坐标的纹理颜色

--- LearnOpenGL/3.Texture/3.3.shader.fs
+++ LearnOpenGL/3.Texture/3.3.shader.fs
@@ -2,7 +2,8 @@
 out vec4 FragColor;
 
 in vec3 ourColor;
-in vec2 TexCoord;
+in vec2 BoxTexCoord;
+in vec2 FaceTexCoord;
 
 uniform sampler2D ourTexture1;
 uniform sampler2D ourTexture2;
@@ -10,5 +11,5 @@ uniform sampler2D ourTexture2;
 void main()
 {
 //    FragColor = mix(texture(ourTexture2, TexCoord), texture(ourTexture1, TexCoord) * vec4(ourColor, 1.0), 0.8);
-    FragColor = mix(texture(ourTexture2, TexCoord), texture(ourTexture1, TexCoord), 0.8);
+    FragColor = mix(texture(ourTexture1, BoxTexCoord), texture(ourTexture2, FaceTexCoord), 0.2);
 }
--- LearnOpenGL/3.Texture/3.3.shader.vs
+++ LearnOpenGL/3.Texture/3.3.shader.vs
@@ -2,13 +2,16 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 layout (location = 2) in vec2 aTexCoord;
+layout (location = 3) in vec2 bTexCoord;
 
 out vec3 ourColor;
-out vec2 TexCoord;
+out vec2 BoxTexCoord;
+out vec2 FaceTexCoord;
 
 void main()
 {
     gl_Position = vec4(aPos, 1.0);
     ourColor = aColor;
-    TexCoord = aTexCoord;
+    BoxTexCoord = aTexCoord;
+    FaceTexCoord =bTexCoord;
 }
